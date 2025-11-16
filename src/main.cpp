55e394add4fd243f136ebfe672612cbc5177bfe1
ShawnSmith7 <shawnsmithsbs7@gmail.com>
2025-11-16 01:29:05 -0500

    added dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 
 int main() {
